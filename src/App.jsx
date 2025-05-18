2422f745ca4d8b749ea8f3994fbf1fbf13471663
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-05-19 01:50:24 +0600

    added header banner and company components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+
 
 
 const App = () => {
   return (
     <div>
-     <h2 className="text-2xl font-bold text-center text-orange-500">Welcome to !! Agency Landing Page</h2>
+      <Header/>
+      <Banner/>
     </div>
   );
 };
