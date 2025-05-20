7372581d12758e6889312eb7fc00439dba89eea0
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-05-20 22:06:52 +0600

    added footer section also but i need to broken the each component in smaller

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Banner from "./components/Banner";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Pricing from "./components/Pricing/Pricing";
 import Service from "./components/Service";
 import Testimonial from "./components/Testimonial";
+import Trial from "./components/Trial";
 
 
 
@@ -14,6 +17,9 @@ const App = () => {
       <Service/>
       <Testimonial/>
       <Pricing/>
+      <FAQ/>
+      <Trial/>
+      <Footer/>
     </div>
   );
 };
